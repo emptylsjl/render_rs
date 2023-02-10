604f972520384c82ef9443efbf941bcd5211ce85
emptylls <songjiellarry@gmail.com>
2023-02-10 00:17:48 -0800

    format

--- vk_cpp/main.cpp
+++ vk_cpp/main.cpp
@@ -160,31 +160,22 @@ private:
 
     void cleanup() {
         cleanupSwapChain();
-
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-
         vkDestroyRenderPass(device, renderPass, nullptr);
-
         for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
             vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
             vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
             vkDestroyFence(device, inFlightFences[i], nullptr);
         }
-
         vkDestroyCommandPool(device, commandPool, nullptr);
-
         vkDestroyDevice(device, nullptr);
-
         if (enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
-
         vkDestroySurfaceKHR(instance, surface, nullptr);
         vkDestroyInstance(instance, nullptr);
-
         glfwDestroyWindow(window);
-
         glfwTerminate();
     }
 
@@ -455,8 +446,8 @@ private:
     }
 
     void createGraphicsPipeline() {
-        auto vertShaderCode = readFile("shader/vert.spv");
-        auto fragShaderCode = readFile("shader/frag.spv");
+        auto vertShaderCode = readFile("D:/cur/render_rs/vk_cpp/shader/vert.spv");
+        auto fragShaderCode = readFile("D:/cur/render_rs/vk_cpp/shader/frag.spv");
 
         VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
@@ -944,7 +935,8 @@ private:
     }
 };
 
-int main() {
+
+int main(int argc, char** argv) {
     HelloTriangleApplication app;
 
     try {
